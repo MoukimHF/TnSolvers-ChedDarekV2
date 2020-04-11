0e633b12310a1ec12ce5d3c765f6bd5cd8c01021
MoukimHF <moukimhfaid@gmail.com>
2020-04-11 15:27:08 +0200

    All the Work
    All the work

---
+++ us.php
@@ -0,0 +1,65 @@
+<section class="bg-other text-center" id="about-us">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-12 col-lg-6">
+                    <div class="content-wrapper mr-0 ml-auto text-right">
+                        <span class="font-weight-bold font-size-xxlarge">
+                        Nous sommes une équipe de 3 personnes
+                    </span>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="content-wrapper ml-0 pt-2 mr-auto text-left">
+                        La mission nous a réunis pour fournir un accès sûr et rapide à l'assistance de quartier pour les personnes âgées sans accès à Internet pendant la pandémie de corona. Nous sommes convaincus que tout est possible ensemble!
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-4 col-lg-3 mb-3 mt-3">
+                    <img src="img/team/anas.jpg" class="img-team" style="height: 120px; width: 120px; border-radius: 50%">
+                    <div class="text-center">
+                        <h4>Anas Chaibi</h4>
+                        <span class="d-block font-weight-bold">Etudiant à l Insat</span>
+                        <span class="d-block">
+                          Tech lover
+                      </span>
+                        <span class="d-block">
+                          <a class="about-social" <i
+                                  class="fab fa-linkedin-in"></i></a>
+                          <a class="about-social" href="https://github.com/anasch07"><i
+                                  class="fab fa-github"></i></a>
+                      </span>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-lg-3 mb-3 mt-3">
+                    <img src="img/team/moukim.jpg" class="img-team" style="height: 120px; width: 120px; border-radius: 50%">
+                    <div class="text-center">
+                        <h4>Moukim</h4>
+                        <span class="d-block font-weight-bold">Etudiant FSEGN</span>
+                        <span class="d-block">JS lover
+                        
+                      </span>
+                        <span class="d-block">
+                        <a class="about-social" href="https://www.linkedin.com/in/moukim"><i
+                                class="fab fa-linkedin-in"></i></a>
+                                <a class="about-social" href="https://github.com/MoukimHF/"><i class="fab fa-github"></i></a>
+                    </span>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-lg-3 mb-3 mt-3">
+                    <img src="img/team/hayder.jpg" style="height: 120px; width: 120px; border-radius: 50%">
+                    <div class="text-center">
+                        <h4>Hayder hedfi</h4>
+                        <span class="d-block font-weight-bold">Etudiant</span>
+                        <span class="d-block"></span>
+                        <span class="d-block">
+                        <a class="about-social" href="https://www.linkedin.com/in/benedikt-bleuel-81a18b1a5/"><i
+                                class="fab fa-linkedin"></i></a>
+                        <a class="about-social" href="https://github.com/hayderhedfi/"><i class="fab fa-github"></i></a>
+                      </span>
+                    </div>
+                </div>
+
+
+            </div>
+    </section>
